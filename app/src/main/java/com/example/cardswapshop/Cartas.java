e0cfa3614d691bb1170a58610cc53cc76330c2cc
ozulejo <ozulejo@gmail.com>
2023-05-24 00:46:55 +0200

    Layout de RecyclerView para Mis Cartas

--- app/src/main/java/com/example/cardswapshop/Cartas.java
+++ app/src/main/java/com/example/cardswapshop/Cartas.java
@@ -1,6 +1,8 @@
 package com.example.cardswapshop;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,6 +16,8 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.cardswapshop.dto.response.CardResponse;
+import com.example.cardswapshop.dto.response.CardsListView;
 import com.example.cardswapshop.dto.response.ListCardsReponse;
 import com.example.cardswapshop.dto.response.TokenResponse;
 import com.example.cardswapshop.helpers.Globals;
@@ -21,14 +25,17 @@ import com.google.gson.Gson;
 
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Cartas extends AppCompatActivity {
 
     Button CartAtrasBtn;
-
+    private RecyclerView cartRecView;
     Globals sharedData = Globals.getInstance();
+
+    private ArrayList<CardsListView> cardList;
 
     private RequestQueue queue;
 
@@ -36,6 +43,7 @@ public class Cartas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cartas);
+        cardList = new ArrayList<>();
         queue = Volley.newRequestQueue(Cartas.this);
 
         String url = "http://10.0.2.2:8080/api/v1/card";
@@ -45,9 +53,10 @@ public class Cartas extends AppCompatActivity {
             public void onResponse(JSONObject response) {
                 Gson gson = new Gson();
                 ListCardsReponse listCardsReponse = gson.fromJson(response.toString(), ListCardsReponse.class);
-
-
-
+                cardList = generateListObject(listCardsReponse);
+                cartRecView=findViewById(R.id.CartRecView);
+                LinearLayoutManager linearLayoutManager = new LinearLayoutManager( Cartas.this);
+                cartRecView.setLayoutManager(linearLayoutManager);
             }
         }, new Response.ErrorListener() {
             @Override
@@ -72,5 +81,18 @@ public class Cartas extends AppCompatActivity {
             Intent intent = new Intent(Cartas.this, Inicio.class);
             startActivity(intent);
         });
+    }
+
+    public ArrayList<CardsListView> generateListObject(ListCardsReponse response){
+        ArrayList <CardsListView> cards = new ArrayList<>();
+        for(CardResponse c : response.getCards()){
+            CardsListView card = new CardsListView();
+            card.setName(c.getName());
+            card.setCardNumber(c.getCard_number());
+            card.setCategory(c.getCollection().getName());
+            card.setImage(c.getImage());
+            cards.add(card);
+        }
+        return cards;
     }
 }
