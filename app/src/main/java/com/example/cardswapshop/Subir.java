6aef35369fdb3d019d1c5b4579a735679871c2d5
ozulejo <ozulejo@gmail.com>
2023-05-25 22:21:26 +0200

    subir

--- app/src/main/java/com/example/cardswapshop/Subir.java
+++ app/src/main/java/com/example/cardswapshop/Subir.java
@@ -2,11 +2,14 @@ package com.example.cardswapshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Base64;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -15,21 +18,48 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.cardswapshop.dto.request.CreateCardRequest;
+import com.example.cardswapshop.dto.response.CollectionResponse;
+import com.example.cardswapshop.dto.response.ListCollectionsResponse;
+import com.example.cardswapshop.helpers.Globals;
+import com.google.gson.Gson;
 
 import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Stack;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.widget.Toast;
 
 public class Subir extends AppCompatActivity {
 
-    String[] collections= {"","Pokemon","Digimon","La liga", "Magic"};
+    Globals sharedData = Globals.getInstance();
+
     Button UpCancelBtn, UpGaleryBtn, UpCamBtn, UpSubirBtn;
     Spinner spinner;
 
+    Context context;
+
     EditText UpName, UpNum;
+
+    List<String> collections;
+    private HashMap<String,String> categoriesCode;
+    String categoryId;
 
     private ImageView imageViewUp;
     private static final int CAMERA_REQUEST = 1888;
@@ -41,7 +71,7 @@ public class Subir extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subir);
-
+        context = this;
         UpName = findViewById(R.id.UpNameTxt);
         UpNum = findViewById(R.id.UpNumTxt);
 
@@ -58,7 +88,7 @@ public class Subir extends AppCompatActivity {
                 collections);
         spinner.setAdapter(adapter);
 
-
+        getCategories();
         UpCamBtn = (Button) this.findViewById(R.id.UpCamBtn);
         UpCamBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,6 +98,19 @@ public class Subir extends AppCompatActivity {
             }
         });
         this.imageViewUp = (ImageView) this.findViewById(R.id.imageViewUp);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
+                categoryId = categoriesCode.get(collections.get(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+
+            }
+        });
 
 
         queue = Volley.newRequestQueue(Subir.this);
@@ -87,7 +130,52 @@ public class Subir extends AppCompatActivity {
     }
     public void processFormFields() throws JSONException{
 
-        String url = "";
+        String url = "http://10.0.2.2:8080/api/v1/card";
+
+
+        CreateCardRequest upRequest = new CreateCardRequest();
+        upRequest.setName(UpName.getText().toString());
+        upRequest.setCollection_id(Long.parseLong(categoryId));
+        upRequest.setCard_number(UpNum.getText().toString());
+        imageViewUp.buildDrawingCache();
+        Bitmap bitmap = ((BitmapDrawable) imageViewUp.getDrawable()).getBitmap();
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+        byte[] byteArray = byteArrayOutputStream.toByteArray();
+        String base64String = Base64.encodeToString(byteArray, Base64.NO_WRAP);
+
+
+
+        upRequest.setImage(base64String);
+        System.out.println(base64String);
+
+
+        Gson gson = new Gson();
+        JSONObject req = new JSONObject(gson.toJson(upRequest));
+        System.out.println(req.toString());
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, req, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Gson gson = new Gson();
+                UpName.setText("");
+                UpNum.setText("");
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(Subir.this, "Error al subir", Toast.LENGTH_LONG).show();
+            }
+        }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Authorization", "Bearer " + sharedData.getValue());
+                return params;
+            }
+        };
+        queue.add(request);
     }
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -97,7 +185,7 @@ public class Subir extends AppCompatActivity {
         }
 
         if (requestCode == SELECT_PHOTO && resultCode == RESULT_OK && data != null &&
-        data.getData() != null){
+                data.getData() != null){
             Uri uri = data.getData();
             try {
                 Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
@@ -119,5 +207,51 @@ public class Subir extends AppCompatActivity {
         });
         this.imageViewUp = (ImageView) this.findViewById(R.id.imageViewUp);
 
+    }
+
+    private void getCategories(){
+        String url = "http://10.0.2.2:8080/api/v1/collection";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Gson gson = new Gson();
+
+                ListCollectionsResponse collectionsResponse = gson.fromJson(response.toString(), ListCollectionsResponse.class);
+                collections = getListOfCollections(collectionsResponse);
+                ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(context,
+                        android.R.layout.simple_spinner_item, collections);
+                dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner.setAdapter(dataAdapter);
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(Subir.this, "Error al cargar cartas", Toast.LENGTH_LONG).show();
+            }
+        }
+        )
+        {
+            @Override
+            public Map<String , String> getHeaders() throws AuthFailureError {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("Authorization", "Bearer " + sharedData.getValue());
+                return params;
+            }
+        };
+        queue.add(request);
+    }
+
+    public List<String> getListOfCollections(ListCollectionsResponse collectionsResponse){
+        List<String> collections = new ArrayList<>();
+        collections.add("todas");
+        categoriesCode.put("todas","0");
+        for (CollectionResponse c : collectionsResponse.getCollections()){
+            collections.add(c.getName());
+            categoriesCode.put(c.getName(),c.getId().toString());
+        }
+        return collections;
     }
 }
